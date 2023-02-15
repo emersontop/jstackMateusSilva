0be86a98f5671138ddc3b85b67567a95a674eaa1
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2023-02-15 09:05:46 -0300

    atualização de estudos

--- f009/src/index.js
+++ f009/src/index.js
@@ -1,6 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((request, response)=>{
+    console.log(`Request Method: ${request.method}|Endpoint:${request.url}`);
+
+    if(request.url ==='/users'&&)
+
     response.writeHead(200,{'contente-type':'text/html'});
     response.end('<h1>Hello, world!</h1>');
 });
